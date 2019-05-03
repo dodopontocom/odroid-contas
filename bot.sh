05a0d113ac543c7269b094c3f51fd2c68b8b6183
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 09:14:20 -0300

    Create bot.sh

---
+++ bot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+
+#sleep para funcionar melhor no startup do sistema
+sleep 10
+
+# Importando API
+BASEDIR=$(dirname $0)
+echo ${BASEDIR}
+source ${BASEDIR}/ShellBot.sh
+logs=${BASEDIR}/logs
+
+# Token do bot
+bot_token=$(cat ${BASEDIR}/.token)
+
+# Inicializando o bot
+ShellBot.init --token "$bot_token" --monitor --flush
+
+while :
+do
+	ShellBot.getUpdates --limit 100 --offset $(ShellBot.OffsetNext) --timeout 30
+	
+	for id in $(ShellBot.ListUpdates)
+	do
+	(
+		if [[ ${message_entities_type[$id]} == bot_command ]]; then
+			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/start" )" ]]; then
+				start
+			fi
+		fi	
+	) & 
+	done
+done
+#FIM
