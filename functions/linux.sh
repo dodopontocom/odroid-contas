b8159fcad7fe4627149c93424be039381511e743
Rodolfo Neto <rodolfo.neto@gft.com>
2019-05-03 13:12:32 -0300

    ok

--- functions/linux.sh
+++ functions/linux.sh
@@ -9,7 +9,7 @@ linux.cmd() {
   array[0]="/linux"
   cmd=${array[@]:1}
   if [[ ! -z ${cmd} ]]; then
-    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(${cmd})" --parse_mode markdown
+    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(${cmd})"
 	else
 		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Usage: ${array[0]} <command>"
 	fi
