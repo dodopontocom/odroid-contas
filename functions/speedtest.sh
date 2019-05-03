76989ed368e97c3cbcfb0f9fcfd114736852f0ee
rodolfotiago <rodolfotiago@gmail.com>
2019-05-03 20:37:25 +0000

    commit

--- functions/speedtest.sh
+++ functions/speedtest.sh
@@ -4,8 +4,8 @@ BASEDIR=$(dirname $0)
 txt=${BASEDIR}/texts/start.txt
 
 speedtest.check() {
-  [[ -f /tmp/test ]] rm -vfr /tmp/test
-  docker run --rm -i python:alpine sh -c "pip install speedtest-cli ; speedtest-cli --bytes --simple" > /tmp/test
-  message=$(tail -3 /tmp/test)
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Aguarde alguns instantes, estou verificando a velocidade da minha internet..."
+  docker run --rm -i python:alpine sh -c "pip install speedtest-cli ; speedtest-cli --bytes --simple" > /tmp/test.txt
+  message=$(tail -3 /tmp/test.txt)
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
 }
