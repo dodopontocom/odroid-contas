7bb61b1e054af5b040ef849ffaba79bac272cc6b
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 10:52:14 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -10,5 +10,5 @@ start.sendGreetings() {
   else
     message+=${message_from_id}
   fi
-  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text '*$(echo -e "${message}")*'
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
 }
