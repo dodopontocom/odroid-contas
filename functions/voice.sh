544f22e738a04a21e1aa3a2f5f04293b46df8ea6
Rodolfo Neto <rodolfo.neto@gft.com>
2019-05-03 15:23:35 -0300

    test

--- functions/voice.sh
+++ functions/voice.sh
@@ -9,15 +9,9 @@ voice.convert() {
   array[0]="/linux"
   message=${array[@]:1}
 
-  file_path=/home/ubuntu
-  file_name=${file_path}/voice.wav
-
   if [[ ! -z ${message} ]]; then
-    #espeak -vpt+f5 --stdout "${voice_command}" > /tmp/voice.ogg
-    echo "${message}"
-    docker run -i --rm -v ${file_path}:/data -w /data ozzyjohnson/tts bash -c 'espeak "${message}" --stdout > voice.wav'
-    ls -lrt ${file_name}
-    ShellBot.sendVoice --chat_id ${message_chat_id[$id]} --voice @/root/file.wav
+    docker run -i --rm -v /home/ubuntu:/data -w /data ozzyjohnson/tts bash -c 'espeak "test 1 2 3" --stdout > voice.ogg'
+    ShellBot.sendVoice --chat_id ${message_chat_id[$id]} --voice @/home/ubuntu/voice.ogg
   else
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Please, envie um texto" --parse_mode markdown
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text '`/voice <texto qualquer>`' --parse_mode markdown
