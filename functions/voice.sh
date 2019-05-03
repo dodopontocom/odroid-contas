6c0c05d2ad83f12c6b356d12fd6c0b5f5b616d74
Rodolfo Neto <rodolfo.neto@gft.com>
2019-05-03 15:38:08 -0300

    aaaah

--- functions/voice.sh
+++ functions/voice.sh
@@ -11,7 +11,7 @@ voice.convert() {
 
   if [[ ! -z ${message} ]]; then
     [[ -f /tmp/voice ]] || mkdir /tmp/voice
-    docker run -it --rm -e "MESSAGE=$message" -v /tmp/voice/:/data -w /data ozzyjohnson/tts bash -c 'export; echo "${MESSAGE}" > text.txt; cat text.txt; espeak -f text.txt --stdout > voice.ogg'
+    docker run -i --rm -e "MESSAGE=$message" -v /tmp/voice/:/data -w /data ozzyjohnson/tts bash -c 'export; echo "${MESSAGE}" > text.txt; cat text.txt; espeak -f text.txt --stdout > voice.ogg'
     #docker run -i --rm -e "MESSAGE=$message" -v /tmp/voice/:/data -w /data ozzyjohnson/tts bash -c 'espeak "${MESSAGE}" --stdout > voice.ogg'
     ShellBot.sendVoice --chat_id ${message_chat_id[$id]} --voice @/tmp/voice/voice.ogg
   else
