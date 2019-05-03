d07f9c1607bf2aa0c3c2124c78afe597647dabe9
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 10:04:19 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -5,12 +5,14 @@ BASEDIR=$(dirname $0)
 source ${BASEDIR}/functions/buttons.sh
 
 start.sendGreetings() {
-
+  keyboard1=buttons.inLine.keyboard1
   message="olá "
   if [[ ! -z $message_from_first_name ]]; then
     message+=${message_from_first_name}
   else
     message+=${message_from_id}
   fi
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "*Pois não ${message_from_first_name} ...*" \
+							--reply_markup "$keyboard1"
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "*$(echo -e "${message}")*"
 }
