e91aeaf7b0f33cd1c50ae75d2fcbd70f949684fb
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 10:57:39 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #
 BASEDIR=$(dirname $0)
+txt=${BASEDIR}/texts/start.txt
 
 start.sendGreetings() {
-  message="olá "
+  message="*olá *"
   if [[ ! -z $message_from_first_name ]]; then
     message+=${message_from_first_name}
   else
     message+=${message_from_id}
   fi
-  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
 }
 
 #start/ajuda function
 #start_command(){
 #	ShellBot.sendChatAction --chat_id ${message_chat_id[$id]} --action typing
-# 	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e $(cat ${BASEDIR}/.start))" --parse_mode markdown
+# 	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e $(cat ${txt}))" --parse_mode markdown
 #	return 0
 #}
