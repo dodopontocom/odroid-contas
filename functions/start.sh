b5a6835dd27df1236aaebaf20353b45ce87c1677
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 09:40:42 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -8,7 +8,5 @@ start.sendGreetings() {
   else
     message+=${message_from_id}
   fi
-  ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} \
-			--text "sorry, você não tem autorização para essa operação..."
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "*$(echo -e "${message}")*"
 }
