a105e5c51bb3e1536e3ab2eb339d1486451ae249
Rodolfo Neto <rodolfo.neto@gft.com>
2019-05-03 14:15:22 -0300

    test

--- functions/voice.sh
+++ functions/voice.sh
@@ -6,15 +6,16 @@ txt=${BASEDIR}/texts/start.txt
 voice.convert() {
   message=$1
   file_path=/home/ubuntu
-  file_name=voice.wav
+  file_name=${file_path}/voice.ogg
 
   if [[ ! -z ${message} ]]; then
     docker run \
     -i \
     --rm \
-    -v ${file_pat}:/data \
+    -v ${file_path}:/data \
     ozzyjohnson/tts \
-    bash -c 'espeak "${message}" --stdout > ${file_name.wav}'
+    bash -c 'espeak "${message}" --stdout > ${file_name}'
+    ShellBot.sendVoice --chat_id ${message_chat_id[$id]} --voice @/tmp/${file_name}
   else
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Please, envie um texto" --parse_mode markdown
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text '`/speedtest <texto qualquer>`' --parse_mode markdown
