280569d8c1b34ab408619fc0366db202e986de68
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 10:59:02 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -11,6 +11,7 @@ start.sendGreetings() {
     message+=${message_from_id}
   fi
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e $(cat ${txt}))" --parse_mode markdown
 }
 
 #start/ajuda function
