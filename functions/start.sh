6f68c143585ff5918bc6ab997a961d5a4fc3f87f
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 10:51:19 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 #
 BASEDIR=$(dirname $0)
-source ${BASEDIR}/ShellBot.sh
-
-start.btn_how() {
-	echo -e '*hello from _how*'
-	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "*$(echo -e '*hello from _how*')"
-	ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]} \
-					--text "TEST_HOW"
-}
-start.btn_hints() {
-	echo -e '*hello from _hints*'
-	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "*$(echo -e '*hello from _hints*')"
-	ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]} \
-					--text "TEST_HINTS"
-}
 
 start.sendGreetings() {
-  botao=''
-  ShellBot.InlineKeyboardButton --button 'botao' --line 1 --text 'Como funciono...' --callback_data 'btn_how'
-  ShellBot.InlineKeyboardButton --button 'botao' --line 2 --text 'Dicas...' --callback_data 'btn_hints'
-  ShellBot.regHandleFunction --function start.btn_how --callback_data btn_how
-  ShellBot.regHandleFunction --function start.btn_hints --callback_data btn_hints
-  keyboard1="$(ShellBot.InlineKeyboardMarkup -b 'botao')"
-  
   message="olá "
   
   if [[ ! -z $message_from_first_name ]]; then
@@ -31,5 +10,5 @@ start.sendGreetings() {
   else
     message+=${message_from_id}
   fi
-  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "*$(echo -e "${message}")*" --reply_markup "$keyboard1" --parse_mode markdown
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text '*$(echo -e "${message}")*'
 }
