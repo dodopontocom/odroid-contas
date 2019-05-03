c247887d0dbdfa424e6415cf03895b48f85b20de
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 10:23:47 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #
-
 BASEDIR=$(dirname $0)
 source ${BASEDIR}/functions/buttons.sh
-echo "---- ${keyboard1}"
+
 start.sendGreetings() {
-  echo "---- ${keyboard1}"
+  botao=''
+  ShellBot.InlineKeyboardButton --button 'botao' --line 1 --text 'Como funciono...' --callback_data 'btn_how'
+  ShellBot.InlineKeyboardButton --button 'botao' --line 2 --text 'Dicas...' --callback_data 'btn_hints'
+  ShellBot.regHandleFunction --function servo_function --callback_data btn_how
+  ShellBot.regHandleFunction --function motion_function --callback_data btn_hints
+  keyboard1="$(ShellBot.InlineKeyboardMarkup -b 'botao')"
+  
   message="olá "
+  
   if [[ ! -z $message_from_first_name ]]; then
     message+=${message_from_first_name}
   else
@@ -14,5 +20,5 @@ start.sendGreetings() {
   fi
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "*Pois não ${message_from_first_name} ...*" \
 							--reply_markup "$keyboard1"
-  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "*$(echo -e "${message}")*"
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "*$(echo -e "${message}")*" --reply_markup "$keyboard1"
 }
