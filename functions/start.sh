304126dceab00e1b75d55f538e1bdf34203ed6f5
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 10:26:36 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -3,12 +3,21 @@
 BASEDIR=$(dirname $0)
 source ${BASEDIR}/functions/buttons.sh
 
+_how() {
+	ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]} \
+					--text "TEST_HOW"
+}
+_hints() {
+	ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]} \
+					--text "TEST_HINTS"
+}
+
 start.sendGreetings() {
   botao=''
   ShellBot.InlineKeyboardButton --button 'botao' --line 1 --text 'Como funciono...' --callback_data 'btn_how'
   ShellBot.InlineKeyboardButton --button 'botao' --line 2 --text 'Dicas...' --callback_data 'btn_hints'
-  ShellBot.regHandleFunction --function servo_function --callback_data btn_how
-  ShellBot.regHandleFunction --function motion_function --callback_data btn_hints
+  ShellBot.regHandleFunction --function _how --callback_data btn_how
+  ShellBot.regHandleFunction --function _hints --callback_data btn_hints
   keyboard1="$(ShellBot.InlineKeyboardMarkup -b 'botao')"
   
   message="olá "
