6fab1cbc524683e347f5b1d1de8e95d3c842d933
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-03 09:21:16 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
 start() {
-  echo "test"
+  message="ola "
+  message+=${message_from_first_name}
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e "${message})"
 }
