852679d4c504e73964a23921ec8c4b669f42d934
rodolfotiago <rodolfotiago@gmail.com>
2019-05-07 11:57:50 +0000

    creating a better lib

--- functions/europe_todo.sh
+++ functions/europe_todo.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #
-BASEDIR=$(dirname $0)
-reminders=${BASEDIR}/texts/europe_todo.txt
-
 source ${BASEDIR}/functions/random.sh
 
 europe_todo_random.message() {
-	local message
-	r_amount=$(random.helper ${reminders})
-	message=$(sed -n "${r_amount}p" < ${reminders})
+	local message reminders random_num
+	reminders=${BASEDIR}/texts/europe_todo.txt
+	random_num=$(random.helper ${reminders})
+	message=$(sed -n "${random_number}p" < ${reminders})
 	if [[ $(echo ${message} | grep -E "\|") ]]; then
   		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message} | cut -d'|' -f1)" --parse_mode markdown
 		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message} | cut -d'|' -f2)" --parse_mode markdown
--- functions/test.sh
+++ functions/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-BASEDIR=$(dirname $0)
 source ${BASEDIR}/functions/random.sh
-
-random.helper ${BASEDIR}/texts/europe_todo.txt
+test.test() {
+    random.helper
+}
