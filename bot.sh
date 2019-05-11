0e9d91375423d12c907eaa016bf87b0558c53f96
rodolfotiago <rodolfotiago@gmail.com>
2019-05-11 21:24:33 +0000

    trip list implementation

--- bot.sh
+++ bot.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #
-
-#sleep para funcionar melhor no startup do sistema
 sleep 10
 
 # Importando API
@@ -18,6 +16,8 @@ source ${BASEDIR}/functions/ping.sh
 source ${BASEDIR}/functions/contas.sh
 source ${BASEDIR}/functions/chat.sh
 source ${BASEDIR}/functions/europe_todo.sh
+source ${BASEDIR}/functions/trip.sh
+source ${BASEDIR}/functions/test.sh
 
 ######################################################################################
 #source <(cat ${BASEDIR}/functions/*.sh)
@@ -32,6 +32,45 @@ bot_token=$(cat ${BASEDIR}/.token)
 # Inicializando o bot
 ShellBot.init --token "$bot_token" --monitor --flush
 
+############### keyboard para o comando contas #######################################
+botao1=''
+ShellBot.InlineKeyboardButton --button 'botao1' --line 1 --text 'Listar' --callback_data 'btn_list'
+ShellBot.InlineKeyboardButton --button 'botao1' --line 1 --text 'Adicionar' --callback_data 'btn_add'
+ShellBot.InlineKeyboardButton --button 'botao1' --line 1 --text 'Remover' --callback_data 'btn_rm'
+ShellBot.regHandleFunction --function contas.list --callback_data btn_list
+ShellBot.regHandleFunction --function contas.add --callback_data btn_add
+ShellBot.regHandleFunction --function contas.rm --callback_data btn_rm
+keyboard1="$(ShellBot.InlineKeyboardMarkup -b 'botao1')"
+#######################################################################################
+#❌
+#✅
+############### keyboard para o comando trip #######################################
+botao2=''
+ShellBot.InlineKeyboardButton --button 'botao2' --line 1 --text 'Listar Todos' --callback_data 'btn_trip_list'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 2 --text 'Listar ✅' --callback_data 'btn_trip_done'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 2 --text 'Listar ❌' --callback_data 'btn_trip_pending'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 4 --text 'Listar Passagens ✅' --callback_data 'btn_trip_passagensV'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 4 --text 'Listar Passagens ❌' --callback_data 'btn_trip_passagensX'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 5 --text 'Listar Trem ✅' --callback_data 'btn_trip_tremV'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 5 --text 'Listar Trem ❌' --callback_data 'btn_trip_tremX'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 6 --text 'Listar Compras ✅' --callback_data 'btn_trip_comprarV'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 6 --text 'Listar Compras ❌' --callback_data 'btn_trip_comprarX'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 7 --text 'Listar Outros ✅' --callback_data 'btn_trip_outrosV'
+ShellBot.InlineKeyboardButton --button 'botao2' --line 7 --text 'Listar Outros ❌' --callback_data 'btn_trip_outrosX'
+ShellBot.regHandleFunction --function list.all --callback_data btn_trip_list
+ShellBot.regHandleFunction --function list.done --callback_data btn_trip_done
+ShellBot.regHandleFunction --function list.pending --callback_data btn_trip_pending
+ShellBot.regHandleFunction --function list.search --callback_data btn_trip_passagensV
+ShellBot.regHandleFunction --function list.search --callback_data btn_trip_passagensX
+ShellBot.regHandleFunction --function list.search --callback_data btn_trip_tremV
+ShellBot.regHandleFunction --function list.search --callback_data btn_trip_tremX
+ShellBot.regHandleFunction --function list.search --callback_data btn_trip_comprarV
+ShellBot.regHandleFunction --function list.search --callback_data btn_trip_comprarX
+ShellBot.regHandleFunction --function list.search --callback_data btn_trip_outrosV
+ShellBot.regHandleFunction --function list.search --callback_data btn_trip_outrosX
+keyboard_trip_checklist="$(ShellBot.InlineKeyboardMarkup -b 'botao2')"
+#######################################################################################
+
 while :
 do
 	
@@ -40,7 +79,7 @@ do
 	for id in $(ShellBot.ListUpdates)
 	do
 	(
-		#ShellBot.watchHandle --callback_data ${callback_query_data[$id]}
+		ShellBot.watchHandle --callback_data ${callback_query_data[$id]}
 		if [[ ${message_entities_type[$id]} == bot_command ]]; then
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/start" )" ]]; then
 				start.sendGreetings
@@ -71,6 +110,9 @@ do
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/reminder" )" ]]; then
 				europe_todo_random.message
+			fi
+			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/trip" )" ]]; then
+				trip.checklist "${message_text[$id]}"
 			fi
 		else
 			chat.hi
