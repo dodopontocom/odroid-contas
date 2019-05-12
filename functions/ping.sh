0a2de1cde200394faed94ad407551b04dceec9ba
rodolfotiago <rodolfotiago@gmail.com>
2019-05-12 20:53:42 -0300

    hello

--- functions/ping.sh
+++ functions/ping.sh
@@ -7,5 +7,4 @@ ping.pong() {
   message="* P o n G 🏓*"
   btn='["🏓 P o n G 🏓"]'								  		
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-    ##--reply_markup "$(ShellBot.ReplyKeyboardMarkup --button 'btn')" \
 }
--- functions/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-source ${BASEDIR}/functions/random.sh
-test.test() {
-    random.helper
-}
