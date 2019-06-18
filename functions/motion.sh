412ada670c104f24d442e1d07ba4b9fbce5f89db
Rodolfo Neto <rodolfotiago@gmail.com>
2019-06-18 06:26:23 -0300

    Update motion.sh

--- functions/motion.sh
+++ functions/motion.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 #
 source ${BASEDIR}/functions/random.sh
-
-speedtest.check() {
-  local message random_file_name
-  message="Aguarde alguns instantes, estou verificando a velocidade da minha internet..."
-  random_file_name=$(random.helper)
-  
-  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
-  
-  docker run --rm -i python:alpine sh -c "pip install speedtest-cli ; speedtest-cli --bytes --simple" > /tmp/${random_file_name}.txt
-  message=$(tail -3 /tmp/${random_file_name}.txt)
-  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
-}
-
-day="$(date +%Y%m%d)"
-detect_folder=/mnt/sdcard/motion/detect/$day
 myId=11504381
 
 motion.get() {
-  local message tmp has_video
+  local message tmp has_video day detect_folder
+  
+  day="$(date +%Y%m%d)"
+  detect_folder=/mnt/sdcard/motion/detect/$day
   
   has_video=($(find $detect_folder -name "*.avi"))
-  echo $has_video
   
   if [[ ! -z $has_video ]]; then
 
     #dar um tempinho a mais para o vídeo poder ser concluido
     sleep 30
-    message="*Motion Detected!!!*"
+    message="*Motion Detected!!!*\n"
+    message+="Enviando o(s) vídeo(s) em instantes..."
 
     for i in ${has_video[@]}; do
       ShellBot.sendMessage --chat_id $myId --text "$(echo -e ${message})" --parse_mode markdown
