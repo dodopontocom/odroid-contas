67060577fd4d82f969ba8b0b8c727fb3a38306dc
Rodolfo Neto <rodolfotiago@gmail.com>
2019-06-25 13:15:42 -0300

    Update motion.sh

--- functions/motion.sh
+++ functions/motion.sh
@@ -4,12 +4,34 @@ source ${BASEDIR}/functions/random.sh
 myId=11504381
 
 motion.get() {
-  local message tmp has_video day detect_folder
+  local message tmp ziptmp has_video day detect_folder
   
   day="$(date +%Y%m%d)"
   detect_folder=/mnt/sdcard/motion/detect/$day
   
-  has_video=($(find $detect_folder -name "*.avi"))
+  #has_video=($(find $detect_folder -name "*.avi"))
+  has_jpg=($(find $detect_folder -name "*.jpg"))
+  
+  if [[ ! -z $has_jpg ]]; then
+
+    #dar um tempinho a mais para o motion seja concluído
+    sleep 30
+    message="*Motion Detected!!!*\n"
+    message+="Enviando o(s) vídeo(s) em instantes..."
+    
+    ziptmp="$(random.helper)"
+    mkdir /tmp/$ziptmp
+    zip /tmp/$ziptmp/$ziptmp.zip $has_jpg 
+    rm -vfr $has_jpg
+    cd /tmp/$ziptmp/
+    unzip $ziptmp.zip
+    ffmpeg -start_number n -i *%d.jpg -vcodec mpeg4 /tmp/$ziptmp.avi
+    cd -
+    
+    ShellBot.sendMessage --chat_id $myId --text "$(echo -e ${message})" --parse_mode markdown
+    ShellBot.sendVideo --chat_id $myId --video @/tmp/$ziptmp.avi
+    
+  fi
   
   if [[ ! -z $has_video ]]; then
 
@@ -27,3 +49,4 @@ motion.get() {
 
   fi
 }
+
