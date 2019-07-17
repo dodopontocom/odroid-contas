c596513b2aa0f45aba501c3a7ebbbd6143cabab0
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-17 14:28:05 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 #
 start.sendGreetings() {
-  local message txt
+  local message txt user_language system
+  
+  system=start
+  user_language=${message_from_language_code}
   txt=${BASEDIR}/texts/start.txt
-  message="*olá *"
+  message=''
+  
   if [[ ! -z $message_from_first_name ]]; then
     message+=${message_from_first_name}
   else
     message+=${message_from_id}
   fi
+  
+  if [[ ${user_language} = "en" ]]; then
+    for i in "$(cat $messages_file | grep ${system} | grep :0: | grep $user_language | cut -d':' -f3)"; do
+      message+="$(cat $messages_file | grep ${system} | grep :${i}: | grep $user_language | cut -d':' -f4)"
+      error_message="$(cat $messages_file | grep ${system} | grep :err: | grep $user_language | cut -d':' -f4)"
+    done
+  else
+    for i in "$(cat $messages_file | grep ${system} | grep :0: | grep pt-br | cut -d':' -f3)"; do
+      message+="$(cat $messages_file | grep ${system} | grep :0: | grep -i pt-br | cut -d':' -f4)"
+      error_message="$(cat $messages_file | grep ${system} | grep :err: | grep -i pt-br | cut -d':' -f4)"
+    done
+  fi
+  
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e $(cat ${txt}))" --parse_mode markdown
 }
