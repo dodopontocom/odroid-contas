6538680448e445f94389139c43b2621d5b1fa69c
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-17 15:06:29 -0300

    Update start.sh

--- functions/start.sh
+++ functions/start.sh
@@ -16,13 +16,13 @@ start.sendGreetings() {
   
   if [[ ${user_language} = "en" ]]; then
     for i in "$(cat $messages_file | grep ${system} | grep :0: | grep $user_language | cut -d':' -f3)"; do
-      message+="$(cat $messages_file | grep ${system} | grep :${i}: | grep $user_language | cut -d':' -f4)"
-      error_message="$(cat $messages_file | grep ${system} | grep :err: | grep $user_language | cut -d':' -f4)"
+      message+="$(cat $messages_file | grep ${system} | grep :${i}: | grep $user_language | cut -d':' -f4-)"
+      error_message="$(cat $messages_file | grep ${system} | grep :err: | grep $user_language | cut -d':' -f4-)"
     done
   else
     for i in "$(cat $messages_file | grep ${system} | grep :0: | grep pt-br | cut -d':' -f3)"; do
-      message+="$(cat $messages_file | grep ${system} | grep :0: | grep -i pt-br | cut -d':' -f4)"
-      error_message="$(cat $messages_file | grep ${system} | grep :err: | grep -i pt-br | cut -d':' -f4)"
+      message+="$(cat $messages_file | grep ${system} | grep :0: | grep -i pt-br | cut -d':' -f4-)"
+      error_message="$(cat $messages_file | grep ${system} | grep :err: | grep -i pt-br | cut -d':' -f4-)"
     done
   fi
   
