56bbfbe4747553e44b23a25548beef1361cae4c2
rodolfotiago <rodolfotiago@gmail.com>
2019-07-17 22:09:21 -0300

    all almost all good

--- functions/voice.sh
+++ functions/voice.sh
@@ -13,7 +13,7 @@ voice.convert() {
   if [[ ! -z ${message} ]]; then
     #docker run -i --rm -e "MESSAGE=$message" -v ${PWD}:/data -w /data ozzyjohnson/tts bash -c 'export; echo "${MESSAGE}" > text.txt; cat text.txt; espeak -f text.txt --stdout > /data/voice.ogg'
     #docker run -i --rm -e "MESSAGE=$message" -v ${PWD}:/data -w /data ozzyjohnson/tts bash -c 'espeak "${MESSAGE}" --stdout > voice.ogg'
-    espeak -vpt "${message}}" --stdout > /tmp/${random_file_name}.ogg
+    espeak -vpt -g10 "${message}}" --stdout > /tmp/${random_file_name}.ogg
     ShellBot.sendVoice --chat_id ${message_chat_id[$id]} --voice @/tmp/${random_file_name}.ogg
   else
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Please, envie um texto para ser sintetizado" --parse_mode markdown
