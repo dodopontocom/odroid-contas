271ecc41d44c928aa5fc7d1f6b6aa556d91fac5c
Rodolfo Neto <rodolfotiago@gmail.com>
2019-08-07 11:04:51 -0300

    Update lotomania.sh

--- functions/lotomania.sh
+++ functions/lotomania.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(dirname $0)
 
 lotomania.sena() {
-  local apiUrl cmd_api jogo message numeros data_hoje
+  local apiUrl cmd_api jogo message numeros data_hoje data
   
   data_hoje=$(date +'%Y-%m-%d')
   apiUrl=https://www.lotodicas.com.br/api
@@ -12,13 +12,17 @@ lotomania.sena() {
   
   if [[ -n ${cmd_api} ]]; then
     
-    message="Sorteio de hoje:"
+    message="✅ Sorteio de hoje:"
     numeros=$(curl -s ${apiUrl}/${jogo} | jq '.sorteio')
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${numeros})" --parse_mode markdown
   else
-    message="Não houve sorteio hoje:"
+    message="❌ Não houve sorteio hoje\n\n"
+    data=$(curl -s ${apiUrl}/${jogo} | jq '.data')
+    numeros=$(curl -s ${apiUrl}/${jogo} | jq '.sorteio')
+    message+="Mostrando sorteio do dia ${data}:"
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${numeros})" --parse_mode markdown
   fi
     
 }
