32ab73d8f30552eaaedd1feb29a772ae3fcb81ea
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-07 13:36:48 -0300

    Update chat.sh

--- functions/chat.sh
+++ functions/chat.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 #
 
+chat_file=/tmp/chat.on
+
+chat.switch
+	local status array message
+
+	status=$1
+	array=(${status})
+	array[0]="/chat"
+	status=${array[@]:1}
+	
+	if [[ ${status[@]} = "on" ]]; then
+		if [[ -f ${chat_file} ]]; then
+			message="Modo falante ligado!"
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+		else
+			touch ${chat_file}
+			message="Modo falante ligado!"
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+		fi
+	elif [[ ${status[@]} = "off" ]]; then
+		if [[ -f ${chat_file} ]]; then
+			rm -f ${chat_file}
+			message="Modo falante desligado!"
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+		else
+			message="Modo falante desligado!"
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+	fi
+}
+
 chat.hi() {
 	local words random_number message
 	
 	words=${BASEDIR}/texts/words.txt
-
 	random_number=$(random.helper ${words})
 	
-	message=$(sed -n "${random_number}p" < ${words})
-	
-	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+	if [[ -f ${chat_file} ]]; then
+		message=$(sed -n "${random_number}p" < ${words})
+		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+	fi
 }
