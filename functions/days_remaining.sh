dbf264fc5069ff7e9517a17202740040fda1843c
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-08 10:03:16 -0300

    Update days_remaining.sh

--- functions/days_remaining.sh
+++ functions/days_remaining.sh
@@ -16,7 +16,7 @@ days.remaining() {
     	message="Use o padrão \`ANO/MES/DIA\`\n"
 	message="Exemplo, quero saber quantos dias faltam para o Natal: \`2019/12/25\` 🦃"
 	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
-    	
+    fi
   else
     message="✅ $(days_from_today "2020-01-14") dias"
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
