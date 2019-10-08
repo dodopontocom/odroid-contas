81aa80c515d9bcf2d62041920ce8fa8b0ec37784
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-08 15:06:30 -0300

    Update linux.sh

--- functions/linux.sh
+++ functions/linux.sh
@@ -99,22 +99,22 @@ linux.reject() {
 linux.cmd() {
   local cmd array message
 
-  if [[ $(tail -1 ${PENDING_PEDIDO} | grep ${message_chat_id[$id]}) ]]; then
+  if [[ $(cat ${PENDING_PEDIDO} | grep ${message_from_id}) ]]; then
     message="Pedido para usar este comando já foi enviado, por favor aguarde."
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
   
-  elif [[ $(cat ${TMP_PEDIDO} | grep ${message_chat_id[$id]}) ]] || [[ $(echo ${NOTIFICATION_IDS[@]} | grep ${message_chat_id[$id]}) ]]; then
+  elif [[ $(cat ${TMP_PEDIDO} | grep ${message_from_id}) ]] || [[ $(echo ${NOTIFICATION_IDS[@]} | grep ${message_from_id}) ]]; then
     cmd=$1
     array=(${cmd})
     array[0]="/linux"
     cmd=${array[@]:1}
   
     if [[ ! -z ${cmd} ]]; then
-      ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(${cmd})"
+      ShellBot.sendMessage --chat_id ${message_from_id} --text "$(${cmd})"
     else
-      ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Usage: ${array[0]} <command>"  
+      ShellBot.sendMessage --chat_id ${message_from_id} --text "Usage: ${array[0]} <command>"  
     fi
   else 
-    linux.ask_register "${message_chat_id[$id]}" "${message_from_first_name}" "${message_from_last_name}"
+    linux.ask_register "${message_from_id}" "${message_from_first_name}" "${message_from_last_name}"
   fi
 }
