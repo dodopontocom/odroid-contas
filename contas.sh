cfe116315a0fc2c731aa98db3d52eccfd6e45459
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-08 14:10:32 -0300

    Update contas.sh

--- contas.sh
+++ contas.sh
@@ -30,8 +30,8 @@ botao1=''
 ShellBot.InlineKeyboardButton --button 'botao1' --line 1 --text 'SIM' --callback_data 'btn_s'
 ShellBot.InlineKeyboardButton --button 'botao1' --line 1 --text 'NAO' --callback_data 'btn_n'
 
-ShellBot.regHandleFunction --function user.add --callback_data btn_s
-ShellBot.regHandleFunction --function user.reject --callback_data btn_n
+ShellBot.regHandleFunction --function linux.add --callback_data btn_s
+ShellBot.regHandleFunction --function linux.reject --callback_data btn_n
 
 keyboard_accept="$(ShellBot.InlineKeyboardMarkup -b 'botao1')"
 ##############################################################################################
