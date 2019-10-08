e83353b8ff3df82b7edabbdc8f35f2d3bcd315d4
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-08 10:12:59 -0300

    Update days_remaining.sh

--- functions/days_remaining.sh
+++ functions/days_remaining.sh
@@ -11,14 +11,15 @@ days.remaining() {
   if [[ ${days[@]} ]]; then
     message="✅ $(helper.date_arithimetic "days_from_today" "${days[@]}") dias"
     if [[ $? -eq 0 ]]; then
-    	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
+    	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     else
     	message="Use o padrão \`ANO/MES/DIA\`\n"
-	message+="Exemplo, quero saber quantos dias faltam para o Natal: /days \`2019/12/25\` 🦃"
-	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
+	message+="Exemplo, quero saber quantos dias faltam para o Natal:"
+	message="/days \`2019/12/25\` 🦃"
+	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     fi
   else
     message="✅ $(helper.date_arithimetic "days_from_today" "2020-01-14") dias"
-    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
+    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
   fi
 }
