af9c21ae36a483f439331a0bf7ae749e3e853eb3
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-08 11:46:45 -0300

    Update days_remaining.sh

--- functions/days_remaining.sh
+++ functions/days_remaining.sh
@@ -10,14 +10,13 @@ days.remaining() {
   
   if [[ ${days[@]} ]]; then
     message="✅ "
-    #message+=$(helper.date_arithimetic "days_from_today" "${days[@]}")
-    message+=$(helper.date_arithimetic "days_from_today" "2019-12-25")
+    message+=$(helper.date_arithimetic "days_from_today" "${days[@]}")
     if [[ $? -eq 0 ]]; then
     	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     else
-    	message="Use o padrão \`ANO-MES-DIA\`\n"
+    	message="Use o padrão \`ANO/MES/DIA\`\n"
 	message+="Exemplo, quero saber quantos dias faltam para o Natal:\n"
-	message+="/days \`2019-12-25\`\n"
+	message+="/days \`2019/12/25\`\n"
 	message+=" 🦃 "
 	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     fi
