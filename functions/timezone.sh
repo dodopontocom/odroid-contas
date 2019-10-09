d3f962cf3e9f6690d41108805e41e63a9ba03324
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-09 15:05:20 -0300

    Update timezone.sh

--- functions/timezone.sh
+++ functions/timezone.sh
@@ -4,12 +4,20 @@
 #✅
 #✖
 
+# Função com bug
+# Função desabilitada
+
 timezone.place() {
 	local array apiUrl place message country
+	
+	message+="Comando em manutenção 😔"
+	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+}
+disabled() {
 	place=$1
 	array=(${place})
-  array[0]="/timezone"
-  place=${array[@]:1}
+  	array[0]="/timezone"
+  	place=${array[@]:1}
 	
 	multi_word_city_capital=$(for i in ${place}; do B=`echo -n "${i:0:1}" | tr "[:lower:]" "[:upper:]"`; echo -n "${B}${i:1} "; done | sed 's/Of/of/g')
 	var=($(echo ${multi_word_city_capital}))
