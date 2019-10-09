b6d105dc8f1115bde6f3ed3a664f6940c80dba52
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-09 12:36:11 -0300

    Update linux.sh

--- functions/linux.sh
+++ functions/linux.sh
@@ -117,8 +117,8 @@ linux.cmd() {
       array[0]="/linux"
       cmd=${array[@]:1}
   
-      if [[ ! -z ${cmd} ]]; then
-        ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(${cmd})"
+      if [[ ! -z ${cmd[@]} ]]; then
+        ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(${cmd[@]})"
       else
         ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Usage: ${array[0]} <command>"  
       fi
