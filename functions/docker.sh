d1979cc92ebfbbca08493680495222197332e1e0
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-09 10:13:25 -0300

    Create docker.sh

---
+++ functions/docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+
+docker.build() {
+  local dockerfile_path message docker_build_cmd
+   
+  dockerfile_path=${1:-${BASEDIR}}
+  
+  message="*Comando docker build em progresso...*"
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+  
+  docker_build_cmd=$(docker build -t contas .)
+  
+  cd ${dockerfile_path}
+  ${docker_build_cmd}
+  if [[ $? -eq 0 ]]; then
+    message="$(docker images | grep contas)"
+    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+  else
+    message="Comando falhou : ("
+    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+  fi
+  
+}
