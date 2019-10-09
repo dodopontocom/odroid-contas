2b484f005cf82f7b9e38d6c99fbea6b35a560f77
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-09 10:58:17 -0300

    Update docker.sh

--- functions/docker.sh
+++ functions/docker.sh
@@ -3,18 +3,16 @@
 
 docker.build() {
   set +f
-  local dockerfile_path message docker_build_cmd
+  local dockerfile_path message
   
   dockerfile_path=${1:-${BASEDIR}}
   
   message="*Comando docker build em progresso...*"
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
   
-  docker_build_cmd=$(docker build -t botcontas .)
-  
   cd ${dockerfile_path}
     
-  ${docker_build_cmd}
+  docker build -t botcontas .
   if [[ $? -eq 0 ]]; then
     message="$(docker images | grep -- botcontas)"
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
@@ -22,5 +20,6 @@ docker.build() {
     message="Comando falhou : ("
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
   fi
+  cd -
   set -f
 }
