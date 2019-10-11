527a9b45bed9d582fe8be84f578f98f37b9548bc
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-11 13:08:09 -0300

    Update contas.sh

--- contas.sh
+++ contas.sh
@@ -166,7 +166,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Madri | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Madri | cut -d',' -f4)\n\n"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/14"
+				days.remaining "1 2020/01/14"
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/dublin" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
@@ -175,7 +175,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Dublin | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Dublin | cut -d',' -f4)"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/16"
+				days.remaining "1 2020/01/16"
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/liverpool" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
@@ -184,7 +184,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Liverpool | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Liverpool | cut -d',' -f4)"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/18"
+				days.remaining "1 2020/01/18"
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/londres" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
@@ -193,7 +193,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Londres | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Londres | cut -d',' -f4)"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/18"
+				days.remaining "1 2020/01/18"
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/berlim" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
@@ -202,7 +202,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Berlim | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Berlim | cut -d',' -f4)"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/21"
+				days.remaining "1 2020/01/21"
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/amsterdam" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
@@ -211,7 +211,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Amsterdam | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Amsterdam | cut -d',' -f4)"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/22"
+				days.remaining "1 2020/01/22"
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/bruxelas" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
@@ -220,7 +220,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Bruxelas | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Bruxelas | cut -d',' -f4)"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/23"
+				days.remaining "1 2020/01/23"
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/paris" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
@@ -229,7 +229,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Paris | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Paris | cut -d',' -f4)"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/25"
+				days.remaining "1 2020/01/25"
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/veneza" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
@@ -238,7 +238,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Veneza | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Veneza | cut -d',' -f4)"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/27"
+				days.remaining "1 2020/01/27"
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/roma" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
@@ -247,7 +247,7 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Roma | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Roma | cut -d',' -f4)"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				days.remaining "1" "2020/01/28"
+				days.remaining "1 2020/01/28"
 			fi
 		else
 			# Conversa aleatória com o bot #
