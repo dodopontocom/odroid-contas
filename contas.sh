f7d97a0058f4d4c8ebced4d0b86c9e6988a34082
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-11 12:51:23 -0300

    Update contas.sh

--- contas.sh
+++ contas.sh
@@ -166,8 +166,8 @@ do
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Madri | cut -d',' -f3)\n"
 				message+="$(cat ${BASEDIR}/texts/trip_cities.csv | grep Madri | cut -d',' -f4)\n\n"
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-				message="⚠️ Atenção! Faltam $(days.remaining "/days 2020/01/14") dias 📅 para essa viagem!"
-				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+				message="⚠️ 📅 Faltam "
+				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(days.remaining /days 2020/01/14)" --parse_mode markdown
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/dublin" )" ]]; then
 				message="$(echo -e ${message_base})\n\n"
