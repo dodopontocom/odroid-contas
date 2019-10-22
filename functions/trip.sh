81ba773075cc082bbd2726266ca5645c1f803278
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-22 18:44:29 +0000

    ok

--- functions/trip.sh
+++ functions/trip.sh
@@ -124,7 +124,7 @@ trip.btn_loc() {
     keyboard_name=$1
     place=$2
 
-	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${place})" --reply_markup "$keyboard_GRU"
+	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${place})" --reply_markup "$keyboard_name"
 }
 
 trip.all_cities() {
