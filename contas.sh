18cbf0f6623b7afb159de6aff7ff9f7972356360
Rodolfo Neto <rodolfotiago@gmail.com>
2019-10-23 09:16:56 -0300

    Update contas.sh

--- contas.sh
+++ contas.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 
-#BASEDIR=$(dirname $0)
 BASEDIR="$(cd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1 && pwd)"
 # Importante utils, script que contem o setup de inicialização do bot
 source ${BASEDIR}/functions/utils.sh
