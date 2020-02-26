450628b431077879862cb21e3b8d1b5db3857ef1
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-26 14:23:04 -0300

    Update lista_compras.sh

--- functions/lista_compras.sh
+++ functions/lista_compras.sh
@@ -7,8 +7,7 @@ function exibir_lista(){
 
         # Cria o botão 'voltar'.
         ShellBot.InlineKeyboardButton --button botao --text '📝' --callback_data 'item_comprado' --line 1
-        ShellBot.InlineKeyboardButton --button botao --text '👎' --callback_data 'item_cancelar' --line 1
-        ShellBot.InlineKeyboardButton --button botao --text 'precos 🔍' --callback_data 'item_valor' --line 1
+        ShellBot.InlineKeyboardButton --button botao --text '🔍' --callback_data 'item_valor' --line 1
         # Anexa o botão a mensagem.
         ShellBot.sendMessage    --chat_id ${message_chat_id[$id]} \
                 --text "*${item}*" \
