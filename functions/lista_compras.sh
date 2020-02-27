c477c377338b4178f295154bb816d93bb2d1f31c
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-27 09:23:56 -0300

    Update lista_compras.sh

--- functions/lista_compras.sh
+++ functions/lista_compras.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-function exibir_lista(){
-        local botao item
-        botao=
+listar.compras(){
+        local item
         item=$1
 
-        #ShellBot.InlineKeyboardButton --button botao --text '✅' --callback_data 'item_comprado' --line 1
-        #ShellBot.InlineKeyboardButton --button botao --text 'preços 🔍' --callback_data 'item_valor' --line 1
-
         ShellBot.sendMessage    --chat_id ${message_chat_id[$id]} \
-                --text "*${item}*" \
+                                --text "*${item}*" \
                                 --parse_mode markdown \
                                 --reply_markup "$keyboard_compras"
 }
-
-#--reply_markup "$(ShellBot.InlineKeyboardMarkup --button botao)"
