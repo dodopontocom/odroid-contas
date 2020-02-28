9db1a7ef0b3116ec785ee9e0f3535d0f4964610e
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-28 17:39:48 -0300

    Update contas.sh

--- contas.sh
+++ contas.sh
@@ -55,7 +55,7 @@ do
 					listar.apagar
 					;;
 				item_valor)
-					listar.precos "${message_text}"
+					listar.precos
                 			;;
                 esac
 		if [[ ${message_entities_type[$id]} == bot_command ]]; then
