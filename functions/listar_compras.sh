05443f8a889c02830123d88835ec673f35f09c69
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-28 16:32:48 -0300

    Update listar_compras.sh

--- functions/listar_compras.sh
+++ functions/listar_compras.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+#botao_itens=''
+#ShellBot.InlineKeyboardButton --button 'botao_itens' --text "${item} ✅" --callback_data 'item_comprado' --line 1
+#ShellBot.InlineKeyboardButton --button 'botao_itens' --text "preços 🔍" --callback_data 'item_valor' --line 1
+#keyboard_itens="$(ShellBot.InlineKeyboardMarkup -b 'botao_itens')"
+
 listar.compras(){
         local item
         item=$1
+        
+        botao_itens=''
+        ShellBot.InlineKeyboardButton --button 'botao_itens' --text "${item} ✅" --callback_data 'item_comprado' --line 1
+        ShellBot.InlineKeyboardButton --button 'botao_itens' --text "preços 🔍" --callback_data 'item_valor' --line 1
+        keyboard_itens="$(ShellBot.InlineKeyboardMarkup -b 'botao_itens')"
 
         ShellBot.deleteMessage --chat_id ${message_chat_id[$id]} --message_id ${message_message_id[$id]}
         ShellBot.sendMessage    --chat_id ${message_chat_id[$id]} \
-                                --text "*${item}*" \
+                                ##--text "*${item}*" \
                                 --parse_mode markdown \
-                                --reply_markup "$keyboard_compras"
+                                #--reply_markup "$keyboard_compras"
+                                --reply_markup "$keyboard_itens"
 }
 
 listar.apagar(){
