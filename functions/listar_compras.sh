ffe2df70ba8915a3aa9069905b6a334f63847793
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-28 16:16:09 -0300

    Update listar_compras.sh

--- functions/listar_compras.sh
+++ functions/listar_compras.sh
@@ -10,3 +10,17 @@ listar.compras(){
                                 --parse_mode markdown \
                                 --reply_markup "$keyboard_compras"
 }
+
+listar.apagar(){
+        
+        ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]}
+        ShellBot.deleteMessage  --chat_id ${callback_query_message_chat_id[$id]} \
+                                --message_id ${callback_query_message_message_id[$id]}
+}
+
+listar.precos(){
+
+        ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]}
+        ShellBot.sendMessage  --chat_id ${callback_query_message_chat_id[$id]} \
+      				--text "Opção em construção..."
+}
