0a3a6f8ee0e8bac54b0429e31b9575982e923136
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-28 17:03:35 -0300

    Update listar_compras.sh

--- functions/listar_compras.sh
+++ functions/listar_compras.sh
@@ -5,13 +5,13 @@ listar.compras(){
         item=$1
         
         botao_itens=''
-        ShellBot.InlineKeyboardButton --button 'botao_itens' --text "${item} ✅" --callback_data 'item_comprado' --line 1
+        ShellBot.InlineKeyboardButton --button 'botao_itens' --text "✅" --callback_data 'item_comprado' --line 1
         ShellBot.InlineKeyboardButton --button 'botao_itens' --text "preços 🔍" --callback_data 'item_valor' --line 1
         keyboard_itens="$(ShellBot.InlineKeyboardMarkup -b 'botao_itens')"
 
         ShellBot.deleteMessage --chat_id ${message_chat_id[$id]} --message_id ${message_message_id[$id]}
         ShellBot.sendMessage    --chat_id ${message_chat_id[$id]} \
-                                --text "." \
+                                --text "${item}" \
                                 --parse_mode markdown \
                                 --reply_markup "$keyboard_itens"
 }
