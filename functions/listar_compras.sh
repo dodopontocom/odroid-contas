e7d4648ed612443898fdece32f705946bf44ae7a
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-28 16:37:32 -0300

    Update listar_compras.sh

--- functions/listar_compras.sh
+++ functions/listar_compras.sh
@@ -16,7 +16,7 @@ listar.compras(){
 
         ShellBot.deleteMessage --chat_id ${message_chat_id[$id]} --message_id ${message_message_id[$id]}
         ShellBot.sendMessage    --chat_id ${message_chat_id[$id]} \
-                                ##--text "*${item}*" \
+                                --text "*${item}*" \
                                 --parse_mode markdown \
                                 #--reply_markup "$keyboard_compras"
                                 --reply_markup "$keyboard_itens"
