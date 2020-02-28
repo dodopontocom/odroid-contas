ab3073ba3a54247c4a819c687b3911d236d148b8
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-28 16:12:13 -0300

    Update listar_compras.sh

--- functions/listar_compras.sh
+++ functions/listar_compras.sh
@@ -4,6 +4,7 @@ listar.compras(){
         local item
         item=$1
 
+        ShellBot.deleteMessage --chat_id ${message_chat_id[$id]} --message_id ${message_message_id[$id]}
         ShellBot.sendMessage    --chat_id ${message_chat_id[$id]} \
                                 --text "*${item}*" \
                                 --parse_mode markdown \
