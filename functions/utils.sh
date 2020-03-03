6c52d561c2ccbaddf71a5d3b610504690fc0efa3
Rodolfo Neto <rodolfotiago@gmail.com>
2020-03-03 09:50:34 -0300

    Update utils.sh

--- functions/utils.sh
+++ functions/utils.sh
@@ -4,33 +4,11 @@
 
 ################################# START - Carregando todas as funções #################################
 test -f ${BASEDIR}/.definitions.sh && source ${BASEDIR}/.definitions.sh
-source ${BASEDIR}/functions/helper.sh
 
-source ${BASEDIR}/functions/start.sh
-source ${BASEDIR}/functions/speedtest.sh
-source ${BASEDIR}/functions/voice.sh
-source ${BASEDIR}/functions/linux.sh
-source ${BASEDIR}/functions/selfie.sh
-source ${BASEDIR}/functions/ping.sh
-source ${BASEDIR}/functions/chat.sh
-source ${BASEDIR}/functions/trip.sh
-source ${BASEDIR}/functions/motion.sh
-source ${BASEDIR}/functions/timezone.sh
-source ${BASEDIR}/functions/offline.sh
-source ${BASEDIR}/functions/lotomania.sh
-source ${BASEDIR}/functions/record_alive.sh
-source ${BASEDIR}/functions/stats.sh
-source ${BASEDIR}/functions/bot_reset.sh
-source ${BASEDIR}/functions/dodrones.sh
-source ${BASEDIR}/functions/docker.sh
-source ${BASEDIR}/functions/days_remaining.sh
-source ${BASEDIR}/functions/servo_play.sh
-source ${BASEDIR}/functions/disk.sh
-source ${BASEDIR}/functions/options.sh
-source ${BASEDIR}/functions/my_ip.sh
-source ${BASEDIR}/functions/precos.sh
-source ${BASEDIR}/functions/listar_compras.sh
-
+function_list=($(find ${BASEDIR}/functions -name "*.sh" | grep -v "utils"))
+for f in ${function_list[@]}; do
+    source ${f}
+done
 ################################# END - Carregando todas as funções #################################
 
 # Saber se tem o telegram token e ao menos um id de adminitrador exportado como variável de ambiente do sistema
