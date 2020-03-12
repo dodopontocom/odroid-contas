534ec0e53e00affaeec2993140ef470ea77af823
Rodolfo Neto <rodolfotiago@gmail.com>
2020-03-12 13:59:34 -0300

    Update lotomania.sh

--- functions/lotomania.sh
+++ functions/lotomania.sh
@@ -4,25 +4,18 @@
 
 # Usa a API da lotomania para pegar o último sorteio da megasena
 lotomania.sena() {
-  local cmd_api jogo message numeros data_hoje data
+  local jogo message numeros
   
-  data_hoje=$(date +'%Y-%m-%d')
-  jogo=mega-sena
-  cmd_api=$(curl -s ${LOTOMANIA_API_URL}/${jogo} | jq '.data' | grep ${data_hoje})
+  jogo=mega_sena
+  numeros=$(curl -s ${LOTOMANIA_API_URL}/${jogo}/results/last?token=${LOTODICAS_TOKEN} | jq '.data.drawing.draw')
   
-  if [[ -n ${cmd_api} ]]; then
-    
-    message="✅ Sorteio de hoje:"
-    numeros=$(curl -s ${LOTOMANIA_API_URL}/${jogo} | jq '.sorteio')
+  if [[ -n ${numeros} ]]; then
+    message="✅ Números do último sorteio:"
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${numeros})" --parse_mode markdown
   else
-    message="❌ Até o momento não houve sorteio de hoje\n\n"
-    data=$(curl -s ${LOTOMANIA_API_URL}/${jogo} | jq '.data')
-    numeros=$(curl -s ${LOTOMANIA_API_URL}/${jogo} | jq '.sorteio')
-    message+="Mostrando sorteio do dia *${data}*:"
+    message="❌ Erro ao tentar buscar úlitmo sorteio"
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${numeros})" --parse_mode markdown
   fi
     
 }
