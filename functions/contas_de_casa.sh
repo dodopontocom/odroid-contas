8ef4659deff2f6f1813429ab2a3ab19cf72c039a
rodolfotiago <rodolfotiago@gmail.com>
2020-03-24 10:13:30 -0300

    test

--- functions/contas_de_casa.sh
+++ functions/contas_de_casa.sh
@@ -6,24 +6,24 @@ contas.show_keyboard() {
     botao_contas=''
 
     cada_conta=($(while read line; do echo "${line}" | cut -d',' -f2; done < ${BOT_CONTAS_LIST}))
-    cada_linha=()
+    declare -A cada_linha
     quant_contas=$(cat ${BOT_CONTAS_LIST} | wc -l)
     for i in $(seq 1 ${quant_contas}); do
         if [[ ${i} -le 3 ]]; then
-            cada_linha+=(1)
+            cada_linha+=(['1']=${i})
         elif [[ ${i} -le 6 ]]; then
-            cada_linha+=(2)
+            cada_linha+=(['2']=${i})
         elif [[ ${i} -le 9 ]]; then
-            cada_linha+=(3)
+            cada_linha+=(['3']=${i})
         elif [[ ${i} -le 12 ]]; then
-            cada_linha+=(4)
+            cada_linha+=(['4']=${i})
         elif [[ ${i} -le 15 ]]; then
-            cada_linha+=(3)
+            cada_linha+=(['5']=${i})
         fi
     done
     echo "------------ ${cada_linha[@]}"
     for c in ${cada_conta[@]}; do
-        ShellBot.InlineKeyboardButton --button 'botao_contas' --text "${c}" --callback_data 'item_conta' --line 1
+        ShellBot.InlineKeyboardButton --button 'botao_contas' --text "${c}" --callback_data 'item_conta' --line ${cada_linha["${c}"]}
     done
 
     keyboard_contas="$(ShellBot.InlineKeyboardMarkup -b 'botao_contas')"
