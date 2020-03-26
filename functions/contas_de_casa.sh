f765831965801480235f604c8044cd9c0a471bec
rodolfotiago <rodolfotiago@gmail.com>
2020-03-26 14:10:49 -0300

    fix bug

--- functions/contas_de_casa.sh
+++ functions/contas_de_casa.sh
@@ -46,10 +46,10 @@ contas.text_return() {
     local conta
 
     conta=$1
-    vencimento="$(cat ${BOT_CONTAS_LIST} | cut -d',' -f1)"
+    vencimento="$(cat ${BOT_CONTAS_LIST} | grep ${conta} | cut -d',' -f1)"
     days=$(helper.date_arithimetic "days_from_today" "$(cat ${BOT_CONTAS_LIST} | grep ${conta} | cut -d',' -f1)")
-    type_pay="$(cat ${BOT_CONTAS_LIST} | cut -d',' -f4)"
-    _is_payed="$(cat ${BOT_CONTAS_LIST} | cut -d',' -f3)"
+    type_pay="$(cat ${BOT_CONTAS_LIST} | grep ${conta} | cut -d',' -f4)"
+    _is_payed="$(cat ${BOT_CONTAS_LIST} | grep ${conta} | cut -d',' -f3)"
     if [[ "${_is_payed}" != "-" ]]; then
         pay_message="Foi Paga em ${_is_payed}.\nVencimento: ${vencimento}"
     else
