1573a13f6bab757112c15bfa773df9c71ed7de80
Rodolfo Neto <rodolfotiago@gmail.com>
2020-03-31 14:50:54 -0300

    Update contas.sh

--- contas.sh
+++ contas.sh
@@ -60,11 +60,26 @@ do
 			item_comprado) listar.apagar ;;
 			item_valor) listar.preco ;;
 			
-			'contas.NATURGY'|'contas.CARRO'|'contas.IPTUV'|'contas.IPTUD'|'contas.POXNET' \
-					|'contas.CPFL'|'contas.SAAE'|'contas.CONDOMINIOD'|'contas.CONDOMINIOV' \
-					|'contas.NUBANKR'|'contas.NUBANKT'|'contas.RENNER'|'contas.CARREFOUR'|'contas.VIVOR' \
-					|'contas.VIVOT'|'contas.IGTI'|'contas.ITAUR'|'contas.ITAUT'|'contas.IPVA' \
-					|'contas.VIVOTSIM'|'contas.VIVOTNAO') contas.start ;;
+			'contas.NATURGY'|'contas.NATURGYSIM'|'contas.NATURGYNAO' \
+				|'contas.CARRO'|'contas.CARROSIM'|'contas.CARRONAO' \
+				|'contas.IPTUV'|'contas.IPTUVSIM'|'contas.IPTUVNAO' \
+				|'contas.IPTUD'|'contas.IPTUDSIM'|'contas.IPTUDNAO' \
+				|'contas.POXNET'|'contas.POXNETSIM'|'contas.POXNETNAO' \
+				|'contas.CPFL'|'contas.CPFLSIM'|'contas.CPFLNAO' \
+				|'contas.SAAE'|'contas.SAAESIM'|'contas.SAAENAO' \
+				|'contas.CONDOMINIOD'|'contas.CONDOMINIODSIM'|'contas.CONDOMINIODNAO' \
+				|'contas.CONDOMINIOV'|'contas.CONDOMINIOVSIM'|'contas.CONDOMINIOVNAO' \
+				|'contas.NUBANKR'|'contas.NUBANKRSIM'|'contas.NUBANKRNAO' \
+				|'contas.NUBANKT'|'contas.NUBANKTSIM'|'contas.NUBANKTNAO' \
+				|'contas.RENNER'|'contas.RENNERSIM'|'contas.RENNERNAO' \
+				|'contas.CARREFOUR'|'contas.CARREFOURSIM'|'contas.CARREFOURNAO' \
+				|'contas.VIVOR'|'contas.VIVORSIM'|'contas.VIVORNAO' \
+				|'contas.VIVOT'|'contas.VIVOTSIM'|'contas.VIVOTNAO' \
+				|'contas.IGTI'|'contas.IGTISIM'|'contas.IGTINAO' \
+				|'contas.ITAUR'|'contas.ITAURSIM'|'contas.ITAURNAO' \
+				|'contas.ITAUT'|'contas.ITAUTSIM'|'contas.ITAUTNAO' \
+				|'contas.IPVA'|'contas.IPVASIM'|'contas.IPVANAO' \
+					) contas.start ;;
 
 			'lotodicas.sena'|'lotodicas.lotofacil'|'lotodicas.quina'|'lotodicas.duplasena' \
 					|'lotodicas.lotomania'|'lotodicas.timemania'|'lotodicas.diasorte') lotodicas.get ;;
