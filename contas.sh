1d07b2eb79ae90a4c93175b22c3f2bc6668df530
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-21 15:03:51 +0000

    new

--- contas.sh
+++ contas.sh
@@ -73,7 +73,7 @@ do
 			item_valor) listar.preco ;;
             _concluir) listar.concluir ;;
             _concluir_sim) listar.sim ;;
-            _concluir_nao) listar.nao ;;
+            _concluir_nao) listar.go_botoes ;;
 
 			'lotodicas.sena'|'lotodicas.lotofacil'|'lotodicas.quina'|'lotodicas.duplasena' \
 					|'lotodicas.lotomania'|'lotodicas.timemania'|'lotodicas.diasorte') lotodicas.get ;;
