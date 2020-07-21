9f13ebc9cf40e025914abad1ec3c6c7771314510
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-21 15:15:40 +0000

    new

--- functions/mercado.sh
+++ functions/mercado.sh
@@ -16,17 +16,19 @@ listar.compras(){
         else
             echo "${_WARN},${item}" >> ${file_list}_lock
         fi
-                
-        botao_itens=''
-        ShellBot.InlineKeyboardButton --button 'botao_itens' --text "${_OK}" --callback_data 'item_comprado' --line 1
-        ShellBot.InlineKeyboardButton --button 'botao_itens' --text "preços ${_LUPA}" --callback_data 'item_valor' --line 1
-        keyboard_itens="$(ShellBot.InlineKeyboardMarkup -b 'botao_itens')"
 
-        ShellBot.deleteMessage --chat_id ${message_chat_id[$id]} --message_id ${message_message_id[$id]}
-        ShellBot.sendMessage    --chat_id ${message_chat_id[$id]} \
-                                --text "*${item}*" \
-                                --parse_mode markdown \
-                                --reply_markup "$keyboard_itens"
+        listar.go_shopping
+                
+        # botao_itens=''
+        # ShellBot.InlineKeyboardButton --button 'botao_itens' --text "${_OK}" --callback_data 'item_comprado' --line 1
+        # ShellBot.InlineKeyboardButton --button 'botao_itens' --text "preços ${_LUPA}" --callback_data 'item_valor' --line 1
+        # keyboard_itens="$(ShellBot.InlineKeyboardMarkup -b 'botao_itens')"
+
+        # ShellBot.deleteMessage --chat_id ${message_chat_id[$id]} --message_id ${message_message_id[$id]}
+        # ShellBot.sendMessage    --chat_id ${message_chat_id[$id]} \
+        #                         --text "*${item}*" \
+        #                         --parse_mode markdown \
+        #                         --reply_markup "$keyboard_itens"
 }
 
 listar.apagar(){
