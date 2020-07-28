c63284ad7ea77078d2c6c69da78368d92496c5f2
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-28 17:21:39 +0000

    lib working in progress

--- contas.sh
+++ contas.sh
@@ -8,23 +8,26 @@ source ${BASEDIR}/.definitions.sh
 #TBOTLIB
 source ${BASEDIR}/tbotlib.sh
 
+tbotlib.use.WelcomeMessage
+tbotlib.use.BooleanInlineButton
+
 # Fazer source das urls para uso da função de busca nos editais
 #source ${BASEDIR}/configs/pdfgrep_urls.sh
 # Fazer source da API só depois de baixá-la
 #source ${BASEDIR}/configs/keyboards.sh
 
 #<TODO> jogar essa var em definitions
-logs=${BASEDIR}/logs
+# logs=${BASEDIR}/logs
 
 # Envia notificação de que o bot foi reiniciado
-for i in ${NOTIFICATION_IDS[@]}; do
-	ShellBot.sendMessage --chat_id ${i} --text "🤖 Bot reiniciado ☝️"
-done
+# for i in ${NOTIFICATION_IDS[@]}; do
+# 	ShellBot.sendMessage --chat_id ${i} --text "🤖 Bot reiniciado ☝️"
+# done
 
 #### contas: Criar os itens dos cases dinamicamente
-_CONTAS_ARR=($(cat ${BOT_CONTAS_LIST} | cut -d',' -f5))
-_CONTAS_SIM_ARR=($(cat ${BOT_CONTAS_LIST} | cut -d',' -f6))
-_CONTAS_NAO_ARR=($(cat ${BOT_CONTAS_LIST} | cut -d',' -f7))
+# _CONTAS_ARR=($(cat ${BOT_CONTAS_LIST} | cut -d',' -f5))
+# _CONTAS_SIM_ARR=($(cat ${BOT_CONTAS_LIST} | cut -d',' -f6))
+# _CONTAS_NAO_ARR=($(cat ${BOT_CONTAS_LIST} | cut -d',' -f7))
 #####################################################################
 
 ################## Enviar estatísticas de comandos ##################
@@ -40,234 +43,23 @@ motion_button="motion_camera_switch"
 while :
 do
 	ShellBot.getUpdates --limit 100 --offset $(ShellBot.OffsetNext) --timeout 30
-	
-    if [[ "${callback_query_message_reply_markup_inline_keyboard_callback_data[$id]}" == "tick_to_one.${motion_button}" ]]; then
-        ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "vou ligar o motion..."
-    elif [[ "${callback_query_message_reply_markup_inline_keyboard_callback_data[$id]}" == "tick_to_zero.${motion_button}" ]]; then
-        ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "vou desligar o motion..."
-    fi
-	
-	################## verificar se virou o mes para atualizar contas ##################
-	contas.verifica_mes $(date "+%m")
-	#####################################################################
-	
-	################## verificar espaço em disco ( / ) ##################
-	#disk.warn "/" "90" ${NOTIFICATION_IDS[0]}
-	#####################################################################
 
-	############### Verifica se o bot ficou offline #############################
-	#offline.checker "$(echo ${NOTIFICATION_IDS[@]})" "90"
-	#############################################################################
-
-	### verifica se há arquivos avi do software motion e envia para os admins ###
-	#motion.get
-	#############################################################################
-    
 	for id in $(ShellBot.ListUpdates)
 	do
 	(
 		ShellBot.watchHandle --callback_data ${callback_query_data[$id]}
 
-		### Envia mensagem de boas vindas para novos usuários de grupo ###
-		[[ ${message_new_chat_member_id[$id]} ]] && helper.welcome_message
-		##################################################################
-		if [[ ${message_chat_id} == "${PRECOS_GROUP_ID}" ]] && \
-                [[ ${message_entities_type[$id]} != bot_command ]] && \
-                [[ -z ${callback_query_data[$id]} ]] && \
-                [[ -z ${message_reply_to_message_message_id} ]]; then
-			listar.compras "${message_text}"
-		else
-			chat.hi
-		fi
-
-        if [[ "$(echo ${callback_query_data[$id]} | grep "${_WARN}\|${_OK}\|Refresh")" ]]; then
-            listar.go_botoes
-        fi
+        [[ ${message_new_chat_member_id[$id]} ]] && WelcomeMessage.send --short --message "I like you"
 		
         case ${callback_query_data[$id]} in
-			pdfgrep.reply_itatiba) pdfgrep.reply_itatiba ;;
-			item_comprado) listar.apagar ;;
-			item_valor) listar.preco ;;
-            _concluir) listar.concluir ;;
-            _concluir_sim) listar.sim ;;
-            _concluir_nao) listar.go_shopping ;;
-            tick_to_one.${motion_button}) tick_to_one.bool_button "${motion_button}" ;;
-            tick_to_zero.${motion_button}) tick_to_zero.bool_button "${motion_button}" ;;
-
-			tick_to_true) button.tick_to_true ;;
- 			tick_to_false) button.tick_to_false ;;
-
-			'lotodicas.sena'|'lotodicas.lotofacil'|'lotodicas.quina'|'lotodicas.duplasena' \
-					|'lotodicas.lotomania'|'lotodicas.timemania'|'lotodicas.diasorte') lotodicas.get ;;
-
+            tick_to_false.bool_1) tick_to_false.bool_button bool_1 ;;
+            tick_to_true.bool_1) tick_to_true.bool_button bool_1 ;;
 		esac
-		for c in ${_CONTAS_ARR[@]}; do
-			case ${callback_query_data[$id]} in
-				${c}) contas.show_contas ;;
-			esac
-		done
-		for s in ${_CONTAS_SIM_ARR[@]}; do
-			case ${callback_query_data[$id]} in
-				${s}) contas.yesno_buttons ;;
-			esac
-		done
-		for n in ${_CONTAS_NAO_ARR[@]}; do
-			case ${callback_query_data[$id]} in
-				${n}) contas.yesno_buttons ;;
-			esac
-		done
-		
-		if [[ ${message_reply_to_message_message_id[$id]} ]]; then
-			case ${message_reply_to_message_text[$id]} in
-				'Pesquisa Itatiba:')
-					pdfgrep.itatiba "${message_text[$id]}"
-				;;
-				'Pesquisa Cerquilho:')
-					pdfgrep.cerquilho "${message_text[$id]}"
-				;;
-                'Valor Total da Compra:')
-                    listar.valor_total "${message_text[$id]}"
-                ;;
-			esac
-		fi
 		
 		if [[ ${message_entities_type[$id]} == bot_command ]]; then
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/switch" )" ]]; then
-				if [[ "${callback_query_message_reply_markup_inline_keyboard_callback_data[$id]}" == "tick_to_one.${motion_button}" ]]; then
-                    tick_to_one.bool_button "${motion_button}"
-                elif [[ "${callback_query_message_reply_markup_inline_keyboard_callback_data[$id]}" == "tick_to_zero.${motion_button}" ]]; then
-                    tick_to_zero.bool_button "${motion_button}"
-                else
-                    init.bool_button "${motion_button}"
-                fi
-                echo "=-=-=- from bot command ${callback_query_message_reply_markup_inline_keyboard_callback_data[$id]}"
-			fi
-            if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/goshopping\|\/verlista" )" ]]; then
-				listar.go_shopping
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/construtora" )" ]]; then
-				site.upload
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/circleci" )" ]]; then
-				circleci.commit "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/cidades" )" ]]; then
-				pdfgrep.keyboard
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/contas" )" ]]; then
-				contas.show_keyboard
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/concursos" )" ]]; then
-				pdfgrep.informativo
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/loterias" )" ]]; then
-				lotodicas.keyboards
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/start" )" ]]; then
-				start.sendGreetings
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/speedtest" )" ]]; then
-				speedtest.check
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/voice" )" ]]; then
-				voice.convert "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/linux" )" ]]; then
-				linux.cmd "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/chat" )" ]]; then
-				chat.switch "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/selfie" )" ]]; then
-				selfie.shot
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/ping" )" ]]; then
-				ping.pong
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/trip" )" ]]; then
-				trip.checklist "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/timezone" )" ]]; then
-				timezone.place "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/days" )" ]]; then
-				days.remaining "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/megasena" )" ]]; then
-				lotomania.sena
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/stats" )" ]]; then
-				stats.verify ${STATS_LOG_PATH}
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/botreset" )" ]]; then
-				bot_reset.bot
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/dodrones" )" ]]; then
-				dodrones.check "${message_chat_id[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/dockerbuild" )" ]]; then
-				docker.build
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/motion" )" ]]; then
-				motion.switch "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/servo" )" ]]; then
-				servo.play "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/disk" )" ]]; then
-				disk.warn "${message_text[$id]}"
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/botip" )" ]]; then
-                my_ip.get
+				BooleanInlineButton.init --true-value "ON" --false-value "OFF" --button-name "bool_1"
             fi
-
-			#### Comandos apenas para nossa viagem de Janeiro
-			#if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/cidades" )" ]]; then
-			#	trip.all_cities
-			#fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/madri" )" ]]; then
-				trip.cities Madri
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/dublin" )" ]]; then
-				trip.cities Dublin
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/liverpool" )" ]]; then
-				trip.cities Liverpool
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/londres" )" ]]; then
-				trip.cities Londres
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/berlim" )" ]]; then
-				trip.cities Berlim
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/amsterdam" )" ]]; then
-				trip.cities Amsterdam
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/bruxelas" )" ]]; then
-				trip.cities Bruxelas
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/paris" )" ]]; then
-				trip.cities Paris
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/veneza" )" ]]; then
-				trip.cities Veneza
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/roma" )" ]]; then
-				trip.cities Roma
-			fi
-			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/baggage" )" ]]; then
-				trip.baggage
-			fi
-			
-		#else
-		#	# Conversa aleatória com o bot #
-		#	#chat.hi
-		#	if [[ ${message_chat_id} == "${PRECOS_GROUP_ID}" ]]; then
-		#		product.search
-		#	else
-		#		chat.hi
-		#	fi
-		#	################################
 		fi
 	) &
 	done
--- tbotlib.sh
+++ tbotlib.sh
@@ -1,41 +1,63 @@
 #!/bin/bash
 
-#VERSION:   build-v0.1-alpha
+#VERSION:build-v0.1-alpha
 
 export API_GIT_URL="https://github.com/shellscriptx/shellbot.git"
 export API_VERSION_RAW_URL="https://raw.githubusercontent.com/shellscriptx/shellbot/master/ShellBot.sh"
+export BOT_LOGS="/home/odroid/BOT_LOGS"
 
+LIB_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null 2>&1 && pwd)
+CALLER_SCRIPT_PATH="${LIB_DIR}/$(basename "${BASH_SOURCE[1]}")"
 LIB_BRANCH="develop"
 LIB_REPO="https://github.com/dodopontocom/tbotlib.git"
-LIB_RAW_URL="https://raw.githubusercontent.com/dodopontocom/tbotlib/master/tbotlib.sh"
-LIBS_FOLDER="${BASEDIR}/tbotlibs/"
+LIB_RAW_URL="https://raw.githubusercontent.com/dodopontocom/tbotlib/${LIB_BRANCH}/tbotlib.sh"
+LIBS_FOLDER="${LIB_DIR}/tbotlibs/"
 tmp_folder=$(mktemp -d)
 
 check_new_version=$(curl -sS ${LIB_RAW_URL} | grep -m1 VERSION | cut -d':' -f2)
-current_version=$(cat ${BASEDIR}/tbotlib.sh | grep -m1 VERSION | cut -d':' -f2)
+current_version=$(cat ${LIB_DIR}/tbotlib.sh | grep -m1 VERSION | cut -d':' -f2)
+echo "[INFO] tbotlib version: '${current_version}'"
 if [[ "${current_version}" != "${check_new_version}" ]]; then
-    echo "[WARN] tbotlib is out of date, we recomend to get new version ${check_new_version}"
-    echo "[INFO] tbotlib script: ${LIB_RAW_URL}"
+    echo "[WARN] new version is available (version: '${check_new_version}')"
+    echo "[INFO] get new version (script: ${LIB_RAW_URL})"
 else
     echo "[INFO] tbotlib is up to date!"
 fi
 
-if [[ ! -d ${BASEDIR}/tbotlibs ]] && \
+if [[ ! -d ${LIB_DIR}/tbotlibs ]] && \
         [[ "$(git config --get remote.origin.url)" != "${LIB_REPO}" ]]; then
+    echo "[INFO] getting tbotlib from original branch: '${LIB_BRANCH}'"
     git clone --quiet --single-branch --branch ${LIB_BRANCH} ${LIB_REPO} ${tmp_folder} > /dev/null
     cp -r ${tmp_folder}/tbotlibs ${LIBS_FOLDER}
     rm -fr ${tmp_folder}
+else
+    echo "[INFO] getting tbotlib from local repo..."
 fi
 
-if [[ ! $(cat ${BASEDIR}/.gitignore | grep tbotlibs) ]] && \
+if [[ ! $(cat ${LIB_DIR}/.gitignore | grep tbotlibs) ]] && \
         [[ "$(git config --get remote.origin.url)" != "${LIB_REPO}" ]]; then
-    echo -e "\n\n#Telegram bot Libs\ntbotlibs\n-" >> ${BASEDIR}/.gitignore
+    echo -e "\n\n#Telegram bot Libs\ntbotlibs\n-" >> ${LIB_DIR}/.gitignore
 fi
 
-libs_list=($(find ${BASEDIR}/tbotlibs -name "*.sh"))
+funcCount() { cat "${1}" | grep "() {$" | grep -v "usage" | wc -l; }
+
+libs_list=($(find ${LIB_DIR}/tbotlibs -name "*.sh" -not -path "*extras/*"))
 for f in ${libs_list[@]}; do
     source ${f}
-    echo "[INFO] Library '$(basename ${f%%.*})' is now loaded. ($(cat ${f} | grep "() {$" | wc -l)) functions you can use from."
+    echo "[INFO] Library '$(basename ${f%%.*})' is now loaded. ($(funcCount "${f}")) functions you can use from."
+done
+
+for ex in $(cat ${CALLER_SCRIPT_PATH} | grep "tbotlib\.use\."); do
+    lib_path="${LIB_DIR}/tbotlibs/extras/$(echo ${ex} | cut -d'.' -f3).sh"
+    source ${lib_path} >/dev/null 2>&1
+    if [[ "$?" -ne "0" ]]; then
+        echo.ERROR "$(echo ${ex} | cut -d'.' -f3) is not a recognized tbotlib"
+        echo.PRETTY "Check our available libs at 'https://github.com/dodopontocom/tbotlib/blob/${LIB_BRANCH}/README.md'"
+        exit -1
+    else        
+        echo.INFO "(manual import) Library '$(echo ${ex} | cut -d'.' -f3)' is now loaded. ($(funcCount "${lib_path}")) functions you can use from."
+        alias "${ex}"="source ${lib_path}"
+    fi
 done
 
 helper.get_api
@@ -43,6 +65,7 @@ exitOnError "Error while trying to download API Shellbot" $?
 helper.validate_vars TELEGRAM_TOKEN
 exitOnError "You must configure and export 'TELEGRAM_TOKEN' variable" $?
 
-source ${BASEDIR}/tbotlibs/API/ShellBot.sh
-ShellBot.init --token "${TELEGRAM_TOKEN}" --monitor --flush
+source ${LIB_DIR}/tbotlibs/API/ShellBot.sh
 echo.SUCCESS "Telegram bot lib is successfully loaded"
+ShellBot.init --token "${TELEGRAM_TOKEN}" --monitor --flush
+echo.SUCCESS "Telegram bot is up and running... enjoy"
