07640e648664de75993acaaeaeca55d52e72adab
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-29 15:03:37 +0000

    go

--- contas.sh
+++ contas.sh
@@ -11,33 +11,6 @@ source ${BASEDIR}/tbotlib.sh
 tbotlib.use.WelcomeMessage
 tbotlib.use.BooleanInlineButton
 
-# Fazer source das urls para uso da função de busca nos editais
-#source ${BASEDIR}/configs/pdfgrep_urls.sh
-# Fazer source da API só depois de baixá-la
-#source ${BASEDIR}/configs/keyboards.sh
-
-#<TODO> jogar essa var em definitions
-# logs=${BASEDIR}/logs
-
-# Envia notificação de que o bot foi reiniciado
-# for i in ${NOTIFICATION_IDS[@]}; do
-# 	ShellBot.sendMessage --chat_id ${i} --text "🤖 Bot reiniciado ☝️"
-# done
-
-#### contas: Criar os itens dos cases dinamicamente
-# _CONTAS_ARR=($(cat ${BOT_CONTAS_LIST} | cut -d',' -f5))
-# _CONTAS_SIM_ARR=($(cat ${BOT_CONTAS_LIST} | cut -d',' -f6))
-# _CONTAS_NAO_ARR=($(cat ${BOT_CONTAS_LIST} | cut -d',' -f7))
-#####################################################################
-
-################## Enviar estatísticas de comandos ##################
-#stats.verify ${STATS_LOG_PATH} "$(echo ${NOTIFICATION_IDS[@]})"
-#####################################################################
-
-####### Verificar recorde de tempo 'vivo' #######
-#record.check "$(echo ${NOTIFICATION_IDS[@]})"
-#################################################
-
 motion_button="motion_camera_switch"
 
 while :
@@ -52,13 +25,13 @@ do
         [[ ${message_new_chat_member_id[$id]} ]] && WelcomeMessage.send --short --message "I like you"
 		
         case ${callback_query_data[$id]} in
-            tick_to_false.bool_1) tick_to_false.bool_button bool_1 ;;
-            tick_to_true.bool_1) tick_to_true.bool_button bool_1 ;;
+            tick_to_false.${motion_button}) BooleanInlineButton.tick_to_false ${motion_button} ;;
+            tick_to_true.${motion_button}) BooleanInlineButton.tick_to_true ${motion_button} ;;
 		esac
 		
 		if [[ ${message_entities_type[$id]} == bot_command ]]; then
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/switch" )" ]]; then
-				BooleanInlineButton.init --true-value "ON" --false-value "OFF" --button-name "bool_1"
+				BooleanInlineButton.init --true-value "ON" --false-value "OFF" --button-name "${motion_button}"
             fi
 		fi
 	) &
